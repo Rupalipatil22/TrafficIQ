71604891944fb647e65a9c8c91c1869321494c3a
Rupalipatil22 <patilrupalipandurang@gmail.com>
2026-09-20 10:06:33 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,8 +3,12 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+
+  base: '/TrafficIQ/',
+
   server: {
     port: 5173,
+
     proxy: {
       '/api': {
         target: 'http://127.0.0.1:8000',
